2ce2fde5b84f7b8bd17e82523fa73fa920edc8b3
harish ashwin raj. s <ashwinharishraj@gmail.com>
2021-04-10 13:25:10 +0530

    hell is better and added cors modules;

--- faqapi/settings.py
+++ faqapi/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = '=&a*(n^+y7h9!6^*gdtl!+j_%j&5@3p@dwm_y$zn_rs(gtwwr1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['immense-springs-58776.herokuapp.com']
+ALLOWED_HOSTS = ['immense-springs-58776.herokuapp.com','127.0.0.1']
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Application definition
@@ -37,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'modelapi'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
